f40b532bdaaf4fd80ed18433b9d9063a4612455b
Adrian Prieto Vuosalmi <aprieto-@c1r12s3.42malaga.com>
2023-03-13 16:43:40 +0100

    ex08

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -1,96 +1,62 @@
-// #include <unistd.h>
-// #include <stdio.h>
-// void ft_putchar(char c)
-// {
-//     write(1, &c, 1);
-// }
-
-// void pintar(int array[], int n)
-// {
-
-//     int i = 0;
-
-//     while (i < n)
-//     {
-//         char c = array[i] + '0';
-//         write(1, &c, 1);
-//         i++;
-//     }
-// }
-
-// void recursive(int n, int array[], int i)
-// {
-//     int j = 0;
-//     while (j <= 9)
-//     {
-//         while (i <= n)
-//         {   printf("i: %i\n",i);
-            
-//             // if (i == 0)
-//             // {
-//             //     array[i] = 0;
-//             // }
-//             // else
-//             // {
-//                 array[0]= 0;
-//                 array[i] = array[i - 1]+1;
-//             // }
-
-//             i++;
-//         }
-        
-//         pintar(array, n);
-//         write(1,", ",2);
-//         j++;
-//     }
-// }
-
-// int ft_print_combn(int n)
-// {
-//     if (n > 10)
-//     {
-//         return 1;
-//     }
-//     int array[n];
-//     recursive(n, array, 0);
-// }
+#include <stdio.h>
 #include <unistd.h>
 
-
-
-void ft_putchar(int c)
-{   
-    c += '0';
-
-    write(1,&c,1);
-}
-void    ft_recursive(int array[], int n, int i)
+void	ft_putchar(char c)
 {
-    while(i < n){
-        if (array[i] == 0){
-            array[i] = 0;
-        } else {
-            array[i] = array[i - 1] + 1;
-        }
-        ft_putchar(array[i]);
-        write(1," ",1);
-        i++;
-        
-    }
-    if (i < 20){
-            ft_recursive(array, n, i);
-        }
+	write(1, &c, 1);
 }
 
-void    ft_print_combn(int n)
+void	pintar(int array[], int n)
 {
-    int array[n];
+	int		i;
+	char	c;
 
-    ft_recursive(array, n, 0);
+	i = 0;
+	while (i < n)
+	{
+		c = array[i] + '0';
+		write(1, &c, 1);
+		i++;
+	}
 }
 
-
-int main(void)
+void	recursive(int n, int array[], int i)
 {
-    ft_print_combn(4);
+	int	j;
+
+	j = 0;
+	while (j <= 9)
+	{
+		while (i <= n)
+		{
+			if (i == 0)
+			{
+				array[i] = 0;
+			}
+			else
+			{
+				array[i] = array[i - 1] + 1;
+			}
+			i++;
+		}
+		pintar(array, n);
+		write(1, ", ", 2);
+		j++;
+	}
+}
+
+int	ft_print_combn(int n)
+{
+	int	array[n];
+
+	if (n > 10)
+	{
+		return (1);
+	}
+	recursive(n, array, 0);
+}
+
+int	main(void)
+{
+	ft_print_combn(9);
 }
